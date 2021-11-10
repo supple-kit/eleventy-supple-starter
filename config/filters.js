a1abb92daecb8023ec36578ce4b12beec50c5bf9
Bram Smulders <source@bram.is>
2021-11-10 17:41:52 +0100

    Added post-list

--- config/filters.js
+++ config/filters.js
@@ -1,10 +1,52 @@
 /**
  * Add Eleventy filters here
  */
+const util = require('util');
 const markdown = require('markdown-it')({
   html: true,
 });
 
+// helpers
+// Stolen from https://stackoverflow.com/a/31615643
+const appendSuffix = (n) => {
+  var s = ['th', 'st', 'nd', 'rd'],
+    v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+};
+
 module.exports = {
   markdown: (value) => markdown.render(value),
+
+  console: (value) => {
+    return util.inspect(value);
+  },
+
+  htmlDateString: (value) => {
+    const dateObject = new Date(value);
+    return dateObject.toISOString();
+  },
+
+  readableDate: (value) => {
+    const dateObject = new Date(value);
+
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    const dayWithSuffix = appendSuffix(dateObject.getDate());
+
+    return `${dayWithSuffix} ${
+      months[dateObject.getMonth()]
+    } ${dateObject.getFullYear()}`;
+  },
 };
--- src/blog/second-post.md
+++ src/blog/second-post.md
@@ -1,10 +1,11 @@
 ---
-title: First post
-description: First post description
-date: 2021-08-10
+title: Second post
+description: Draft
+date: 2021-11-10
 tags:
   - first tag
   - posts with two tags
+draft: true
 ---
 
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, itaque! Dolor quas itaque iure dolorem quam possimus sapiente quia vel optio saepe incidunt temporibus, libero, veniam vero voluptatem quidem impedit!
