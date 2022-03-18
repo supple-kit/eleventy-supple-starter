f02f2517b9395170ffd113d5525d4ce15afccce4
Bram Smulders <source@bram.is>
2022-03-18 18:36:20 +0100

    added RSS plugin

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,13 @@
  * Add Eleventy plugins here
  */
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
 
 module.exports = {
   eleventyNavigation: (eleventyConfig) => {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  },
+  eleventyRss: (eleventyConfig) => {
+    eleventyConfig.addPlugin(pluginRss);
   },
 };
